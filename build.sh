9d8949f6f17056c47ffc0971ee2d3ed27376357d
Kian-Tat Lim <ktl@slac.stanford.edu>
2019-09-16 02:51:01 -0500

    Reorganize and simplify.

---
+++ build.sh
@@ -0,0 +1,7 @@
+export OSPL_VERSION=6.9.0
+export LSSTTS_SAL_VERSION=3.10.0
+export LSSTTS_SALOBJ_VERSION=4.5.0
+export LSSTTS_XML_VERSION=4.2.0
+export LSSTTS_IDL_VERSION=0.2.0
+
+docker-compose build
