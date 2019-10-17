9b0fbbafca2891addb822b6d98997d2eeb5a7fcb
Felipe Menanteau <felipe@illinois.edu>
2019-10-17 10:49:25 -0500

    Updates to start docker headerservice automatically as a service.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ export LSSTTS_SAL_VERSION=3.10.0
 export LSSTTS_SALOBJ_VERSION=4.5.0
 export LSSTTS_XML_VERSION=4.3.0
 export LSSTTS_IDL_VERSION=0.2.0
-export HEADERSERVICE_VERSION=1.0.1
+export HEADERSERVICE_VERSION=1.1.0
 export SALPYTOOLS_VERSION=0.9.7
 export CCS_IDL_LIST="EFD ATHeaderService ATCamera ATArchiver ATPtg ATMCS ATSpectrograph ATTCS ATHexapod"
 docker-compose build
