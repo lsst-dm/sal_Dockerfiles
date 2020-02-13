e57be2765696bf4b6c05f2e6567be22f15ee2eaa
Felipe Menanteau <felipe@illinois.edu>
2020-02-13 14:15:04 -0600

    Change for version 1.4.0

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ export LSSTTS_SAL_VERSION=4.0.0
 export LSSTTS_SALOBJ_VERSION=5.0.0
 export LSSTTS_XML_VERSION=4.6.0
 export LSSTTS_IDL_VERSION=1.0.0
-export HEADERSERVICE_VERSION=1.3.1
+export HEADERSERVICE_VERSION=1.4.0
 export CCS_IDL_LIST="EFD ATHeaderService ATCamera ATArchiver ATPtg ATMCS ATSpectrograph ATTCS ATHexapod"
 docker-compose build salrun salidl salobj atheaderservice
