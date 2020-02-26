74bbfe4f58e97a2c406ef66a086d81a03a616269
Felipe Menanteau <felipe@illinois.edu>
2020-02-26 10:37:44 -0600

    Update to use Py37 using miniconda

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 export OSPL_VERSION=6.9.0
 export LSSTTS_SAL_VERSION=4.0.0
-export LSSTTS_SALOBJ_VERSION=5.0.0
-export LSSTTS_XML_VERSION=4.6.0
-export LSSTTS_IDL_VERSION=1.0.0
+export LSSTTS_SALOBJ_VERSION=5.4.0
+export LSSTTS_XML_VERSION=4.7.0
+export LSSTTS_IDL_VERSION=1.1.0
 export HEADERSERVICE_VERSION=1.4.0
-export CCS_IDL_LIST="EFD ATHeaderService ATCamera ATArchiver ATPtg ATMCS ATSpectrograph ATTCS ATHexapod"
+export MINICONDA_PATH=/opt/miniconda3
+export TAG=v${HEADERSERVICE_VERSION}_salobj_v${LSSTTS_SALOBJ_VERSION}_xml_v${LSSTTS_XML_VERSION}
+export CCS_IDL_LIST="EFD ATHeaderService ATCamera ATArchiver ATPtg ATMCS ATSpectrograph ATTCS ATHexapod CCCamera CCHeaderService"
 docker-compose build salrun salidl salobj atheaderservice
