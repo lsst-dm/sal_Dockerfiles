e96e1673922d34dabe62fe31adeced4c053907e6
Felipe Menanteau <felipe@illinois.edu>
2020-03-09 10:35:56 -0500

    Adding Environment

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ export LSSTTS_IDL_VERSION=1.1.0
 export HEADERSERVICE_VERSION=1.4.0
 export MINICONDA_PATH=/opt/miniconda3
 export TAG=v${HEADERSERVICE_VERSION}_salobj_v${LSSTTS_SALOBJ_VERSION}_xml_v${LSSTTS_XML_VERSION}
-export CCS_IDL_LIST="EFD ATHeaderService ATCamera ATArchiver ATPtg ATMCS ATSpectrograph ATTCS ATHexapod ATDome DIMM CCCamera CCArchiver CCHeaderService"
+export CCS_IDL_LIST="EFD ATHeaderService ATCamera ATArchiver ATPtg ATMCS ATSpectrograph ATTCS ATHexapod ATDome DIMM CCCamera CCArchiver CCHeaderService Environment"
 docker-compose build salrun salidl salobj atheaderservice
