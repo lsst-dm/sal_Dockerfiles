1a6096ee6420113e66f4a6c5bde2c86ad26295e3
Felipe Menanteau <felipe@illinois.edu>
2020-04-29 15:28:55 -0500

    Adding MTPtg and Hexapod

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ export LSSTTS_IDL_VERSION=1.1.3
 export HEADERSERVICE_VERSION=2.1.0
 export MINICONDA_PATH=/opt/miniconda3
 export TAG=v${HEADERSERVICE_VERSION}_salobj_v${LSSTTS_SALOBJ_VERSION}_xml_v${LSSTTS_XML_VERSION}
-export CCS_IDL_LIST="ATHeaderService ATCamera ATArchiver ATPtg ATMCS ATSpectrograph ATHexapod ATDome DIMM CCCamera CCArchiver CCHeaderService Environment"
+export CCS_IDL_LIST="ATHeaderService ATCamera ATArchiver ATPtg MTPtg ATMCS ATSpectrograph ATHexapod Hexapod ATDome DIMM CCCamera CCArchiver CCHeaderService Environment"
 docker-compose build salrun salidl salobj headerservice
