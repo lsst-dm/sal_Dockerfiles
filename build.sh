b89a5ee1cc3842a7f71d02b76f0cb70356e04b8b
Felipe Menanteau <felipe@illinois.edu>
2020-07-20 15:41:24 -0500

    Update for HS 2.3.2

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ export LSSTTS_SAL_VERSION=4.1.1
 export LSSTTS_XML_VERSION=6.0.0
 export LSSTTS_SALOBJ_VERSION=5.15.0
 export LSSTTS_IDL_VERSION=1.2.2
-export HEADERSERVICE_VERSION=2.3.1
+export HEADERSERVICE_VERSION=2.3.2
 export MINICONDA_PATH=/opt/miniconda3
 export TAG=v${HEADERSERVICE_VERSION}_salobj_v${LSSTTS_SALOBJ_VERSION}_xml_v${LSSTTS_XML_VERSION}
 docker-compose build salrun salidl salobj headerservice
