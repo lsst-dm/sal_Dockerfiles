fe247f04925d255dd3a7e8ea9c1f3d667b5f1ff5
Felipe Menanteau <felipe@illinois.edu>
2020-10-15 15:16:41 -0500

    Changes for new version and private docker.

--- build.sh
+++ build.sh
@@ -6,7 +6,15 @@ export LSSTTS_XML_VERSION=6.1.0
 export LSSTTS_SALOBJ_VERSION=6.0.2
 export LSSTTS_IDL_VERSION=2.0.0
 export LSSTTS_IDL_CONDA_VERSION=${LSSTTS_IDL_VERSION}_${LSSTTS_XML_VERSION}_${LSSTTS_SAL_VERSION}
-export HEADERSERVICE_VERSION=2.3.3
+export HEADERSERVICE_VERSION=2.4.0
 export MINICONDA_PATH=/opt/miniconda3
+
+export TAG_TS=v${HEADERSERVICE_VERSION}_salobj_v${LSSTTS_SALOBJ_VERSION}_idl_v${LSSTTS_SALIDL_VERSION}_xml_v${LSSTTS_XML_VERSION}
+export TAG_DM=${LSSTTS_SAL_VERSION}-${LSSTTS_XML_VERSION}-salobj_${LSSTTS_SALOBJ_VERSION}-hs_${HEADERSERVICE_VERSION}
 export TAG=v${HEADERSERVICE_VERSION}_salobj_v${LSSTTS_SALOBJ_VERSION}_xml_v${LSSTTS_XML_VERSION}
 docker-compose build salrun salidl salobj headerservice
+
+echo "Re-taggins as:"
+echo "               ts-dockerhub.lsst.org/headerservice:dm-${TAG_TS}"
+echo "docker tag lsstdm/headerservice:${TAG_DM} ts-dockerhub.lsst.org/headerservice:dm-${TAG_TS}"
+docker tag lsstdm/headerservice:${TAG_DM} ts-dockerhub.lsst.org/headerservice:dm-${TAG_TS}
